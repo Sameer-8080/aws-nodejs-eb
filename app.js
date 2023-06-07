07845f5a4f8487a89b79ce26bad10bfefca23877
Sameer <109897419+Sameer-8080@users.noreply.github.com>
2023-06-07 18:54:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'S3 CloudHub YT channel & Elastic Beanstalk'
+    title: 'Node Js deployment for Elastic-BeanStalk'
     })
     res.status(200).end();
 })
